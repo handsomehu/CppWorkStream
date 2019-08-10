3ba84a7efceb72b8540e18656748f32e97a079e6
trade <leonzh18@hotmail.com>
2019-08-10 11:15:51 +0800

    cpp start

---
+++ AveragePrice/src/average_price.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : average_price.cpp
+// Author      : Leon Zhao
+// Version     : 0.1
+// Copyright   : Initial
+// Description : C++ Journey Begin, Wow!
+//============================================================================
+
+#include <iostream>
+//using namespace std;
+
+int main() {
+	std::cout << "C++ Journey Begin, Wow!" << std::endl;
+	return 0;
+}
