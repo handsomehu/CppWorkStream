bfcc72739152632f6d060562710feb2d13d3d719
trade <leonzh18@hotmail.com>
2019-10-21 22:27:34 +0800

    constructor

--- BookSamples/CppPrimer/CppPrimerC1/Sales_data.cpp
+++ BookSamples/CppPrimer/CppPrimerC1/Sales_data.cpp
@@ -1,5 +1,5 @@
 #include "Sales_data.h"
-
+#include <iostream>
 double Sales_data::avg_price() const
 {
     if (units_sold > 0)
@@ -13,4 +13,30 @@ Sales_data& Sales_data::combine(const Sales_data &rhs)
     revenue +=  rhs.revenue;
     units_sold += rhs.units_sold;
     return *this;
+}
+
+std::istream &read(std::istream &is,Sales_data &item)
+{
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = item.units_sold*price;
+    return is;
+}
+std::ostream &print(std::ostream &os,const Sales_data &item )
+{
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue << std::endl;
+    return os;
+
+}
+
+Sales_data &add(const Sales_data &lhs, const Sales_data &rhs)
+{
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum; // return sum will be copied? otherwise should be destroy?
+}
+
+Sales_data::Sales_data(std::istream &is)
+{
+read(is, *this); // read will read a transaction from is into this object
 }
--- BookSamples/CppPrimer/CppPrimerC1/Sales_data.h
+++ BookSamples/CppPrimer/CppPrimerC1/Sales_data.h
@@ -36,6 +36,11 @@ struct Sales_data {
 	std::string bookNo;
 	unsigned units_sold = 0;
 	double revenue = 0.0;
+    Sales_data() = default;
+    Sales_data(const std::string &s): bookNo(s) { }
+    Sales_data(const std::string &s, unsigned n, double p):
+    bookNo(s), units_sold(n), revenue(p*n) { }
+    Sales_data(std::istream &);
     std::string isbn() const
     {
         return bookNo;
