0ac43c84195e402302277f3832b36b517b20d6dc
trade <leonzh18@hotmail.com>
2019-10-24 18:12:42 +0800

    Oct24

--- BookSamples/CppPrimer/CppPrimerC1/main.cpp
+++ BookSamples/CppPrimer/CppPrimerC1/main.cpp
@@ -7,6 +7,22 @@
 #include <initializer_list>
 #include <fstream>
 #include <forward_list>
+void test_iter()
+{
+    // silly loop to remove even-valued elements and insert a duplicate of odd-valued elements
+    std::vector<int> vi = {0,1,2,3,4,5,6,7,8,9};
+    auto iter = vi.begin(); // call begin, not cbegin because we're changing vi
+    while (iter != vi.end()) {
+    if (*iter % 2)
+    {
+        iter = vi.insert(iter, *iter); // duplicate the current element
+        iter += 2; // advance past this element and the one inserted before it
+    }
+    else
+        iter = vi.erase(iter); // remove even elements
+    // don't advance the iterator; iter denotes the element after the one we erased
+    }
+}
 void test_fwdlist()
 {
     std::forward_list<int> flst = {0,1,2,3,4,5,6,7,8,9};
