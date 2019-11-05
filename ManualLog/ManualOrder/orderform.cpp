050bd00822a2564a3416bfab7cdf1f31e5ed606c
trade <leonzh18@hotmail.com>
2019-11-05 22:13:21 +0800

    sql

--- ManualLog/ManualOrder/orderform.cpp
+++ ManualLog/ManualOrder/orderform.cpp
@@ -13,6 +13,7 @@ OrderForm::OrderForm(QWidget *parent)
     ui->cb_offset->addItems({"开","平"});
     //qDebug() << dbhelper.getMaxId();
     ui->cb_strg->addItems({"DT_IntraDayCommonStrategy","TurtleUseCloseStrategy","JDualThrust_IntraDayStrategy"});
+    ui->cb_exch->addItems({"sqs","dss","zzs"});
 }
 
 OrderForm::~OrderForm()
@@ -23,12 +24,28 @@ OrderForm::~OrderForm()
 
 void OrderForm::on_pb_order_clicked()
 {
-    ui->te_infor->setText("Here we are ordering for " + ui->le_symbol->text()+dbhelper.getMaxId("tradeorders"));
+    QString insertsql,rmk1,rmk2,otime,strname,qty;
+    QString dir,offset,prc,symbol,exc,dt;
+    rmk1 = ui->te_rmk1->toPlainText();
+    rmk2 = ui->te_rmk2->toPlainText();
+    //strname = ui->cb_strg->sele
+    insertsql = "INSERT INTO tradeorders";
+    insertsql+= "(remark2,remark1,ordertime,strategyname,";
+    insertsql+= "orderqty,orderdirection,orderoffset,";
+    insertsql+= "entryprice,contract,exchangeid,orderdate)";
+    insertsql+= "values (:rmk1,:rmk2,:otime,:strname,:qty";
+    insertsql+= ",:dir,:offset,:prc,:symbol,:exc,:dt)";
+    //use binding I have to pass a lots params, so directly use sql string
+
 }
 
 void OrderForm::on_pb_reset_clicked()
 {
     ui->le_symbol->setText("");
     ui->le_price->setText("");
-    ui->te_infor->setText("");
+    ui->te_log->setText("");
+    ui->te_rmk1->setText("");
+    ui->te_rmk2->setText("");
+    //ui->te_infor->setText("");
+
 }
--- ManualLog/build-ManualOrder-Desktop_Qt_5_12_5_GCC_64bit-Debug/ui_orderform.h
+++ ManualLog/build-ManualOrder-Desktop_Qt_5_12_5_GCC_64bit-Debug/ui_orderform.h
@@ -30,22 +30,33 @@ class Ui_OrderForm
 public:
     QWidget *centralwidget;
     QPushButton *pb_order;
-    QTextEdit *te_infor;
+    QTextEdit *te_rmk1;
     QPushButton *pb_reset;
-    QWidget *widget;
+    QWidget *layoutWidget;
     QHBoxLayout *horizontalLayout;
     QVBoxLayout *verticalLayout_2;
     QLabel *label_dir;
     QLabel *label_offset;
     QLabel *label_strg;
+    QLabel *label_exch;
     QLabel *label_symbol;
     QLabel *label_price;
+    QLabel *label_qty;
     QVBoxLayout *verticalLayout;
     QComboBox *cb_Dir;
     QComboBox *cb_offset;
     QComboBox *cb_strg;
+    QComboBox *cb_exch;
     QLineEdit *le_symbol;
     QLineEdit *le_price;
+    QLineEdit *le_qty;
+    QTextEdit *te_rmk2;
+    QTextEdit *te_log;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout_3;
+    QLabel *label_rmk1;
+    QLabel *label_rmk2;
+    QLabel *label_log;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -53,88 +64,135 @@ public:
     {
         if (OrderForm->objectName().isEmpty())
             OrderForm->setObjectName(QString::fromUtf8("OrderForm"));
-        OrderForm->resize(630, 342);
+        OrderForm->resize(778, 521);
         centralwidget = new QWidget(OrderForm);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         pb_order = new QPushButton(centralwidget);
         pb_order->setObjectName(QString::fromUtf8("pb_order"));
-        pb_order->setGeometry(QRect(59, 240, 101, 21));
-        te_infor = new QTextEdit(centralwidget);
-        te_infor->setObjectName(QString::fromUtf8("te_infor"));
-        te_infor->setGeometry(QRect(410, 30, 211, 201));
+        pb_order->setGeometry(QRect(50, 300, 101, 21));
+        te_rmk1 = new QTextEdit(centralwidget);
+        te_rmk1->setObjectName(QString::fromUtf8("te_rmk1"));
+        te_rmk1->setGeometry(QRect(380, 30, 211, 51));
         pb_reset = new QPushButton(centralwidget);
         pb_reset->setObjectName(QString::fromUtf8("pb_reset"));
-        pb_reset->setGeometry(QRect(170, 240, 101, 21));
-        widget = new QWidget(centralwidget);
-        widget->setObjectName(QString::fromUtf8("widget"));
-        widget->setGeometry(QRect(60, 30, 212, 190));
-        horizontalLayout = new QHBoxLayout(widget);
+        pb_reset->setGeometry(QRect(161, 300, 101, 21));
+        layoutWidget = new QWidget(centralwidget);
+        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
+        layoutWidget->setGeometry(QRect(60, 30, 212, 251));
+        horizontalLayout = new QHBoxLayout(layoutWidget);
         horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
         horizontalLayout->setContentsMargins(0, 0, 0, 0);
         verticalLayout_2 = new QVBoxLayout();
         verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
-        label_dir = new QLabel(widget);
+        label_dir = new QLabel(layoutWidget);
         label_dir->setObjectName(QString::fromUtf8("label_dir"));
 
         verticalLayout_2->addWidget(label_dir);
 
-        label_offset = new QLabel(widget);
+        label_offset = new QLabel(layoutWidget);
         label_offset->setObjectName(QString::fromUtf8("label_offset"));
 
         verticalLayout_2->addWidget(label_offset);
 
-        label_strg = new QLabel(widget);
+        label_strg = new QLabel(layoutWidget);
         label_strg->setObjectName(QString::fromUtf8("label_strg"));
 
         verticalLayout_2->addWidget(label_strg);
 
-        label_symbol = new QLabel(widget);
+        label_exch = new QLabel(layoutWidget);
+        label_exch->setObjectName(QString::fromUtf8("label_exch"));
+
+        verticalLayout_2->addWidget(label_exch);
+
+        label_symbol = new QLabel(layoutWidget);
         label_symbol->setObjectName(QString::fromUtf8("label_symbol"));
 
         verticalLayout_2->addWidget(label_symbol);
 
-        label_price = new QLabel(widget);
+        label_price = new QLabel(layoutWidget);
         label_price->setObjectName(QString::fromUtf8("label_price"));
 
         verticalLayout_2->addWidget(label_price);
+
+        label_qty = new QLabel(layoutWidget);
+        label_qty->setObjectName(QString::fromUtf8("label_qty"));
+
+        verticalLayout_2->addWidget(label_qty);
 
 
         horizontalLayout->addLayout(verticalLayout_2);
 
         verticalLayout = new QVBoxLayout();
         verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
-        cb_Dir = new QComboBox(widget);
+        cb_Dir = new QComboBox(layoutWidget);
         cb_Dir->setObjectName(QString::fromUtf8("cb_Dir"));
 
         verticalLayout->addWidget(cb_Dir);
 
-        cb_offset = new QComboBox(widget);
+        cb_offset = new QComboBox(layoutWidget);
         cb_offset->setObjectName(QString::fromUtf8("cb_offset"));
 
         verticalLayout->addWidget(cb_offset);
 
-        cb_strg = new QComboBox(widget);
+        cb_strg = new QComboBox(layoutWidget);
         cb_strg->setObjectName(QString::fromUtf8("cb_strg"));
 
         verticalLayout->addWidget(cb_strg);
 
-        le_symbol = new QLineEdit(widget);
+        cb_exch = new QComboBox(layoutWidget);
+        cb_exch->setObjectName(QString::fromUtf8("cb_exch"));
+
+        verticalLayout->addWidget(cb_exch);
+
+        le_symbol = new QLineEdit(layoutWidget);
         le_symbol->setObjectName(QString::fromUtf8("le_symbol"));
 
         verticalLayout->addWidget(le_symbol);
 
-        le_price = new QLineEdit(widget);
+        le_price = new QLineEdit(layoutWidget);
         le_price->setObjectName(QString::fromUtf8("le_price"));
 
         verticalLayout->addWidget(le_price);
 
+        le_qty = new QLineEdit(layoutWidget);
+        le_qty->setObjectName(QString::fromUtf8("le_qty"));
+
+        verticalLayout->addWidget(le_qty);
+
 
         horizontalLayout->addLayout(verticalLayout);
+
+        te_rmk2 = new QTextEdit(centralwidget);
+        te_rmk2->setObjectName(QString::fromUtf8("te_rmk2"));
+        te_rmk2->setGeometry(QRect(380, 100, 211, 51));
+        te_log = new QTextEdit(centralwidget);
+        te_log->setObjectName(QString::fromUtf8("te_log"));
+        te_log->setGeometry(QRect(380, 160, 211, 51));
+        widget = new QWidget(centralwidget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(290, 30, 82, 191));
+        verticalLayout_3 = new QVBoxLayout(widget);
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        verticalLayout_3->setContentsMargins(0, 0, 0, 0);
+        label_rmk1 = new QLabel(widget);
+        label_rmk1->setObjectName(QString::fromUtf8("label_rmk1"));
+
+        verticalLayout_3->addWidget(label_rmk1);
+
+        label_rmk2 = new QLabel(widget);
+        label_rmk2->setObjectName(QString::fromUtf8("label_rmk2"));
+
+        verticalLayout_3->addWidget(label_rmk2);
+
+        label_log = new QLabel(widget);
+        label_log->setObjectName(QString::fromUtf8("label_log"));
+
+        verticalLayout_3->addWidget(label_log);
 
         OrderForm->setCentralWidget(centralwidget);
         menubar = new QMenuBar(OrderForm);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 630, 27));
+        menubar->setGeometry(QRect(0, 0, 778, 27));
         OrderForm->setMenuBar(menubar);
         statusbar = new QStatusBar(OrderForm);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -153,8 +211,13 @@ public:
         label_dir->setText(QApplication::translate("OrderForm", "Direction", nullptr));
         label_offset->setText(QApplication::translate("OrderForm", "Offset", nullptr));
         label_strg->setText(QApplication::translate("OrderForm", "Strategy", nullptr));
+        label_exch->setText(QApplication::translate("OrderForm", "Exchange", nullptr));
         label_symbol->setText(QApplication::translate("OrderForm", "Symbol", nullptr));
         label_price->setText(QApplication::translate("OrderForm", "Price", nullptr));
+        label_qty->setText(QApplication::translate("OrderForm", "TextLabel", nullptr));
+        label_rmk1->setText(QApplication::translate("OrderForm", "Remark1", nullptr));
+        label_rmk2->setText(QApplication::translate("OrderForm", "Remark2", nullptr));
+        label_log->setText(QApplication::translate("OrderForm", "Log Infor", nullptr));
     } // retranslateUi
 
 };
