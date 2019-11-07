cd866e694eb6d6616637d423ec474da1b29bfd2b
trade <leonzh18@hotmail.com>
2019-11-07 15:34:28 +0800

    trade

---
+++ QtExplore/CtpTradeTest/ctraderhandler.cpp
@@ -0,0 +1,223 @@
+#include "ctraderhandler.h"
+
+CTraderHandler::CTraderHandler()
+{
+
+}
+void CTraderHandler::connect()
+{
+    //创建API实例
+
+    m_ptraderapi = CThostFtdcTraderApi::CreateFtdcTraderApi(".//flow/");
+    //必须提前创建好flow目录，流文件*.con就会在该文件夹下面创建，如果想要区别不同session创建的流水文件名称可见如下示例“.//flow/a_”
+
+    m_ptraderapi->RegisterSpi(this);
+
+    m_ptraderapi->SubscribePublicTopic(THOST_TERT_QUICK);
+
+    m_ptraderapi->SubscribePrivateTopic(THOST_TERT_QUICK); //设置私有流订阅模式
+
+    m_ptraderapi->RegisterFront("tcp://127.0.0.1:41205");
+
+    m_ptraderapi->Init();
+
+    //输出API版本信息
+
+    printf("%s\n", m_ptraderapi->GetApiVersion());
+
+}
+
+void CTraderHandler::release()
+{
+    m_ptraderapi->Release();
+}
+
+void CTraderHandler::login()
+{
+    CThostFtdcReqUserLoginField t = {0};
+
+    std::strcpy(t.BrokerID, "1701");
+
+    std::strcpy(t.UserID, "1701_admin");
+
+    std::strcpy(t.Password, "1701_admin");
+
+    while (m_ptraderapi->ReqUserLogin(&t, 1)!=0)
+        std::this_thread::sleep_for(std::chrono::seconds(1));//Sleep(1000);
+}
+
+//结算单确认
+
+void CTraderHandler::settlementinfoConfirm()
+
+{
+
+    CThostFtdcSettlementInfoConfirmField t = {0};
+
+    std::strcpy(t.BrokerID, "1701");
+
+    std::strcpy(t.InvestorID, "00001");
+
+    while (m_ptraderapi->ReqSettlementInfoConfirm(&t, 2)!=0)
+        std::this_thread::sleep_for(std::chrono::seconds(1));//Sleep(1000);
+
+}
+
+
+
+//报单
+
+void CTraderHandler::orderinsert()
+
+{
+
+    CThostFtdcInputOrderField t = { 0 };
+
+    std::strcpy(t.BrokerID, "9999");
+
+    std::strcpy(t.InvestorID, "00001");
+
+    std::strcpy(t.UserID, "00001");
+
+    t.Direction = THOST_FTDC_D_Buy;
+
+    t.CombOffsetFlag[0] = THOST_FTDC_OF_Open;
+
+    t.CombHedgeFlag[0] = THOST_FTDC_HF_Speculation;
+
+    t.ContingentCondition = THOST_FTDC_CC_Immediately;
+
+    std::strcpy(t.InstrumentID, "sc1807");
+
+    t.ForceCloseReason = THOST_FTDC_FCC_NotForceClose;
+
+    t.LimitPrice = 490;
+
+    t.OrderPriceType = THOST_FTDC_OPT_LimitPrice;
+
+    t.VolumeCondition = THOST_FTDC_VC_AV;
+
+    t.TimeCondition = THOST_FTDC_TC_GFD;
+
+    t.VolumeTotalOriginal = 1;
+
+    std::strcpy(t.OrderRef, "0000001");
+
+    std::strcpy(t.ExchangeID, "INE");
+
+    while (m_ptraderapi->ReqOrderInsert(&t, 3) != 0)
+        std::this_thread::sleep_for(std::chrono::seconds(1));//Sleep(1000);
+
+}
+
+//查询合约
+
+void CTraderHandler::qryInstrument()
+
+{
+
+    CThostFtdcQryInstrumentField t = { 0 };
+
+    std::strcpy(t.ExchangeID, "SHFE");
+
+    std::strcpy(t.InstrumentID, "zn1803");
+
+    while (m_ptraderapi->ReqQryInstrument(&t, 4) != 0)
+        std::this_thread::sleep_for(std::chrono::seconds(1));//Sleep(1000);
+
+}
+
+
+
+//前置连接响应
+
+void CTraderHandler::OnFrontConnected()
+
+{
+
+    printf("OnFrontConnected\n");
+
+}
+
+
+
+//登陆请求响应
+
+void CTraderHandler::OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+
+{
+
+    printf("OnRspUserLogin\n");
+
+}
+
+
+
+//结算单确认响应
+
+void CTraderHandler::OnRspSettlementInfoConfirm(CThostFtdcSettlementInfoConfirmField *pSettlementInfoConfirm, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+
+{
+
+    printf("OnRspSettlementInfoConfirm\n");
+
+}
+
+
+
+//查询合约响应
+
+void CTraderHandler::OnRspQryInstrument(CThostFtdcInstrumentField *pInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+
+{
+
+    printf("OnRspQryInstrument\n");
+
+}
+
+
+//报单通知
+
+void CTraderHandler::OnRtnOrder(CThostFtdcOrderField *pOrder)
+
+{
+
+    printf("OnRtnOrder\n");
+
+}
+
+
+
+//成交通知
+
+void CTraderHandler::OnRtnTrade(CThostFtdcTradeField *pTrade)
+
+{
+
+    printf("OnRtnTrade\n");
+
+}
+
+
+
+//报单录入请求响应
+
+void CTraderHandler::OnRspOrderInsert(CThostFtdcInputOrderField *pInputOrder, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+
+{
+
+    printf("OnRspOrderInsert\n");
+
+}
+
+
+
+//错误应答
+
+void CTraderHandler::OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+
+{
+
+    printf("OnRspError\n");
+
+}
---
+++ QtExplore/CtpTradeTest/ctraderhandler.h
@@ -0,0 +1,45 @@
+#ifndef CTRADERHANDLER_H
+#define CTRADERHANDLER_H
+#include "./libhead/ThostFtdcTraderApi.h"
+//#include "traderApi.h"
+#include <string.h>
+#include <cstring>
+#include <stdio.h>
+#include <memory>
+#include <chrono>
+#include <thread>
+//#include <Windows.h>
+
+class CTraderHandler: public CThostFtdcTraderSpi
+{
+public:
+
+    CTraderHandler();
+    //activat method
+    void connect();
+    void release();
+    void login();
+    void settlementinfoConfirm();
+    void orderinsert();
+    void qryInstrument();
+    //responsive method
+    void OnFrontConnected();
+    void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    void OnRspSettlementInfoConfirm(CThostFtdcSettlementInfoConfirmField *pSettlementInfoConfirm, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    void OnRspQryInstrument(CThostFtdcInstrumentField *pInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    void OnRtnOrder(CThostFtdcOrderField *pOrder);
+    void OnRtnTrade(CThostFtdcTradeField *pTrade);
+    void OnRspOrderInsert(CThostFtdcInputOrderField *pInputOrder, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    void OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+
+private:
+
+    // could use smart point replace
+    // How the api instance the class? via stack or new?
+    CThostFtdcTraderApi *m_ptraderapi;
+
+
+};
+
+#endif // CTRADERHANDLER_H
