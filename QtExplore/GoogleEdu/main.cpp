d8146916a33eb0ee5608719c6c5dfa24c7ab35c0
trade <leonzh18@hotmail.com>
2019-11-10 22:38:34 +0800

    google

---
+++ QtExplore/GoogleEdu/main.cpp
@@ -0,0 +1,11 @@
+//https://developers.google.com/edu/c++/getting-started
+// hello.cpp: Maggie Johnson
+// Description: a program that prints the immortal saying "hello world"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  return 0;
+}
