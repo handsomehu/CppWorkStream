aa30d1966f9c9799863586b2ae62fa3e15c31687
trade <leonzh18@hotmail.com>
2019-11-25 22:21:57 +0800

    bs book

--- BookSamples/CppPL_BS/CppFromBS1/main.cpp
+++ BookSamples/CppPL_BS/CppFromBS1/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-
+int test_si()
+{
+string from, to;
+cin >> from >> to; // get source and target file names
+ifstream is {from}; // input stream for file "from"
+istream_iterator<string> ii {is}; // input iterator for stream
+istream_iterator<string> eos {}; // input sentinel
+ofstream os{to}; // output stream for file "to"
+ostream_iterator<string> oo {os,"\n"}; // output iterator for stream
+vector<string> b {ii,eos}; // b is a vector initialized from input [ii:eos)
+sort(b.begin(),b.end()); // sor t the buffer
+unique_copy(b.begin(),b.end(),oo); // copy buffer to output, discard replicated values
+return !is.eof() || !os; // return error state (§2.2.1, §38.3)
+}
 int main()
 {
     cout << "Hello World!" << endl;
+    test_si();
     return 0;
 }
