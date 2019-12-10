4b854eeca202a2ff250c0acf1a565bd4a73e7fe9
trade <leonzh18@hotmail.com>
2019-12-10 22:16:24 +0800

    bs book

--- BookSamples/CppPL_BS/CppFromBS1/main.cpp
+++ BookSamples/CppPL_BS/CppFromBS1/main.cpp
@@ -19,9 +19,29 @@ sort(b.begin(),b.end()); // sor t the buffer
 unique_copy(b.begin(),b.end(),oo); // copy buffer to output, discard replicated values
 return !is.eof() || !os; // return error state (§2.2.1, §38.3)
 }
+void digits()
+{
+    for(int i = 1;i!=10;++i)
+        cout << static_cast<char>('0'+i);
+    char c = 255;
+    int ci = c;
+    cout << ci << std::endl;
+}
+void g111(char c, signed char sc, unsigned char uc)
+{
+    c = 255; // implementation-defined if plain chars are signed and have 8 bits
+    c = sc; // OK
+    c = uc; // implementation-defined if plain chars are signed and if uc’s value is too large
+    sc = uc; // implementation defined if uc’s value is too large
+    uc = sc; // OK: conversion to unsigned
+    sc = c; // implementation-defined if plain chars are unsigned and if c’s value is too large
+    uc = c; // OK: conversion to unsigned
+
+}
 int main()
 {
     cout << "Hello World!" << endl;
-    test_si();
+    //test_si();
+    digits();
     return 0;
 }
