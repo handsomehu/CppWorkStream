aaee91751e55bc63d46508ff7b3206929fed3fc0
trade <leonzh18@hotmail.com>
2020-01-02 17:58:54 +0800

    simplest start

--- Algo/AlgoExercisers/main.cpp
+++ Algo/AlgoExercisers/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
+int fastsort(std::vector<int>& sorted,int target)
+{
+    int low{0},high{0},mid{0},guess{0};
+    high = sorted.size()-1;
+    //std::cout << high << std::endl;
+    mid = (low+high)/2;
+    while(low <=high)
+    {
+        if (target == sorted[mid])
+            return mid;
+        else if (target < sorted[mid])
+        {
+            high = mid-1;
+            mid = (low+high)/2;
+        }
+        else
+        {
+            low = mid+1;
+            mid = (low+high)/2;
+        }
+
+    }
+    return 0;
+
+
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::cout << "Hello World!" << std::endl;
+    int tofind =0;
+    std::vector<int> v1{1,2,3,4,5,6,7,8,9,10,24,34,36,40,50,56};
+    while(std::cin>>tofind)
+        std::cout<< fastsort(v1,tofind) << std::endl;
     return 0;
 }
