2cc07f55046b7de78e5bf768bc65876bf0b1596c
trade <leonzh18@hotmail.com>
2020-01-05 18:14:59 +0800

    search

--- Algo/AlgoExercisers/main.cpp
+++ Algo/AlgoExercisers/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
-
+#include <list>
+#include <iterator>
+//some example from book grokking algorithms
 int binsort(std::vector<int>& sorted,int target)
 {
+    int cnt{0};
     int low{0},high{0},mid{0},guess{0};
     high = sorted.size()-1;
     //std::cout << high << std::endl;
@@ -10,7 +13,10 @@ int binsort(std::vector<int>& sorted,int target)
     while(low <=high)
     {
         if (target == sorted[mid])
+        {
+            std::cout << "step used:" <<cnt << std::endl;
             return mid;
+        }
         else if (target < sorted[mid])
         {
             high = mid-1;
@@ -21,6 +27,8 @@ int binsort(std::vector<int>& sorted,int target)
             low = mid+1;
             mid = (low+high)/2;
         }
+        cnt+=1;
+
 
     }
     return 0;
@@ -40,7 +48,7 @@ std::vector<int> selectsort1(std::vector<int>& data)
             {
                 temp = data[j];
                 idx = j;
-                std::cout << idx << std::endl;
+                //std::cout << idx << std::endl;
 
             }
         }
@@ -53,6 +61,43 @@ std::vector<int> selectsort1(std::vector<int>& data)
 
 
 }
+std::list<int>::iterator select_search_max(std::list<int> & sample)
+{
+    int idx{0},temp{0};
+    std::list<int>::iterator maxit{sample.begin()};
+
+    for(auto p = sample.begin();p!=sample.end();++p)
+        if (*p>*maxit)
+            maxit = p;
+    return maxit;
+
+}
+void select_search2(std::list<int>& sample)
+{
+    std::list<int> tgt{};
+    std::list<int>::iterator temp{};
+    int scnt = sample.size();
+    for (auto idx = 0;idx != scnt;++idx)
+    {
+        temp = select_search_max(sample);
+        tgt.push_back(*temp);
+        sample.erase(temp);
+    }
+    for(auto x:tgt)
+    {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+}
+
+int fact_test(int val)
+{
+    if (val == 1)
+        return val;
+    else
+        return val*fact_test(val-1);
+}
 int main()
 {
     std::cout << "Hello World!" << std::endl;
@@ -64,5 +109,8 @@ int main()
     for(auto x:st)
         std::cout << x << " ";
     std::cout << std::endl;
+    std::list<int> lt{1,2,3,4,5,6,7,8,9,10,24,34,36,40,50,56};
+    select_search2(lt);
+    std::cout << "factories of 5:" << fact_test(5) << std::endl;
     return 0;
 }
