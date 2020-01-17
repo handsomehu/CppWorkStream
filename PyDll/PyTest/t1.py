b2aafa9443c957cb68a650efc024f2dd9faa231c
trade <leonzh18@hotmail.com>
2020-01-17 14:04:37 +0800

    test

--- PyDll/PyTest/t1.py
+++ PyDll/PyTest/t1.py
@@ -1,4 +1,9 @@
 import pickle 
-filehandler = open("./tx.pkl", 'r') 
-object = pickle.load(filehandler)
-print object
+#filehandler = open("./tx.pkl", 'r') 
+#object = pickle.load(filehandler)
+#print object
+
+
+import datetime
+now = datetime.datetime.now()
+print now.year, now.month, now.day, now.hour, now.minute, now.second
