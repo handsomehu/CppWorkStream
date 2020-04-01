41e7e246a9141ba4863faabba93ee0b5d662ed45
trade <leonzh18@hotmail.com>
2020-04-01 16:58:48 +0800

    add

--- query_settlement/main.cpp
+++ query_settlement/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
-#include <time.h>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 //#include <codecvt>
 #include "tradewrapper.h"
 
@@ -13,33 +15,67 @@ std::vector<std::string>&& getdate()
     std::vector<std::string> dates{};
     struct tm date;
 
-    date.tm_mon = 1;
+    date.tm_mon = 0;
     date.tm_mday = 1;
-    date.tm_year = 2019 - 1900;
+    date.tm_year = 2018-1900;
+    std::ostringstream os;
+    os << std::put_time(date, "%Y%m%d");
+    std::cout << os.str() << std::endl;
 
     time_t end = time(NULL);
 
     for (; mktime(&date) < end; ++date.tm_mday) {
+
         char buffer[16];
 
         strftime(buffer, sizeof(buffer), "%Y%m%d", &date);
-        std::cout << buffer << "\n";
+        std::cout << buffer << "\t";
+
         dates.push_back(std::string(buffer));
+        break;
     }
     return std::move(dates);
+
+}
+
+void getdate(std::vector<std::string>& dates)
+{
+
+    struct tm date;
+
+    date.tm_mon = 0;
+    date.tm_mday = 1;
+    date.tm_year = 2018 - 1900;
+    time_t end = time(NULL);
+
+    for (; mktime(&date) < end; ++date.tm_mday) {
+        char buffer[16];
+        strftime(buffer, sizeof(buffer), "%Y%m%d", &date);
+        std::cout << buffer << "\n";
+        dates.push_back(std::string(buffer));
+        //break;
+    }
+    //return std::move(dates);
 
 }
 
 int main()
 {
     std::vector<std::string> datelist=getdate();
+    std::cout << datelist[0] << std::endl;
+
+
+
+    return 0;
+
     TradeWrapper api("./cfg/j123.json");
     api.connect();
     std::this_thread::sleep_for(std::chrono::seconds(5));
     char resultsets[200001];   // array to hold the result.
     //char oneline[501];
-    for(auto dt:datelist)
+    for(std::string dt:datelist)
     {
+        std::cout << "date is0" << dt << std::endl;
         api.qrySettlement(dt);
         std::this_thread::sleep_for(std::chrono::seconds(2));
         auto rst = api.getsettlements();
@@ -58,10 +94,13 @@ int main()
         char dst_utf8_set[200001] = {0};
         GbkToUtf8(resultsets, strlen(resultsets), dst_utf8_set, sizeof(dst_utf8_set));
         //std::cout << "gbk to utf8: " << dst_utf8_set << std::endl;
+        std::cout << "date is " << dt << std::endl;
         std::string fname = "./data/trade_"+dt+".txt";
         std::ofstream os(fname);
         os << dst_utf8_set;
         std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        break;
 
 
     }
